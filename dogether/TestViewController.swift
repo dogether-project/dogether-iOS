b6b1232319dbdb8fea5498aa732d3798e6299980
seungyooooong <chadange@naver.com>
2025-02-02 16:09:25 +0900

    feat #3: Firebase PushNotice 설정
    1. 앱 APNs 설정 추가
    2. Firebase package 추가
    3. Firebase & PushNoticeManager 초기 설정 추가
    4. checkAuthorization 추가
    5. TestViewController 추가 (FCMToken 확인용)

--- dogether/TestViewController.swift
+++ dogether/TestViewController.swift
@@ -6,22 +6,21 @@
 //
 
 import UIKit
+import FirebaseMessaging
 
 // TODO: 테스트 이후 삭제
 class TestViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let testButton = UIButton(type: .system)
-        testButton.setTitle("API Test Button", for: .normal)
-        
-        testButton.addAction(UIAction(handler: { [weak self] _ in
-            Task {
-                try? await self?.performNetworkRequest()
-            }
+        testButton.setTitle("TCM Token check button", for: .normal)
+
+        testButton.addAction(UIAction(handler: { _ in
+            checkFCMToken()
         }), for: .touchUpInside)
-        
+
         testButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(testButton)
         NSLayoutConstraint.activate([
@@ -29,10 +28,11 @@ class TestViewController: UIViewController {
             testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
-    
-    @MainActor
-    func performNetworkRequest() async throws {
-        let response: CreateGroupResponse = try await NetworkManager.shared.request(.createGroup(createGroupRequest: CreateGroupRequest()))
-        print(response)
+}
+
+// TODO: 추후에 로그인 뷰에서 세부 구현
+func checkFCMToken() {
+    Messaging.messaging().token { token, _ in
+        if let token = token { print("checked FCM Token: \(token)") }
     }
 }
--- dogether/Utility/Manager/PushNoticeManager.swift
+++ dogether/Utility/Manager/PushNoticeManager.swift
@@ -5,19 +5,38 @@
 //  Created by seungyooooong on 2/1/25.
 //
 
-import Foundation
+import UIKit
 import UserNotifications
+import FirebaseMessaging
 
 class PushNoticeManager: NSObject, UNUserNotificationCenterDelegate {
     static let shared = PushNoticeManager()
     private override init() {
         super.init()
-        Task { await setupNotificationDelegate() }
+        Task {
+            await setupNotificationDelegate()
+            try await checkAuthorization()
+        }
     }
     
     private func setupNotificationDelegate() async {
         await MainActor.run {
             UNUserNotificationCenter.current().delegate = self
+        }
+    }
+    
+    private func checkAuthorization() async throws {
+        let userNoti = UNUserNotificationCenter.current()
+        let settings = await userNoti.notificationSettings()
+        switch settings.authorizationStatus {
+        case .notDetermined:
+            if try await userNoti.requestAuthorization(options: [.alert, .badge, .sound]) {
+                await UIApplication.shared.registerForRemoteNotifications()
+            }
+        case .denied:
+            break   // TODO: 알림이 꺼져있다는 Alert 등으로 사용자를 설정 앱으로 유도
+        default:    // MARK: .authorized, .provisional, .ephemeral
+            break
         }
     }
     
@@ -27,6 +46,8 @@ class PushNoticeManager: NSObject, UNUserNotificationCenterDelegate {
         willPresent notification: UNNotification,
         withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
     ) {
+        print("foreground notification: \(notification)")
+        
         completionHandler([.banner, .sound])
     }
     
@@ -36,6 +57,8 @@ class PushNoticeManager: NSObject, UNUserNotificationCenterDelegate {
         didReceive response: UNNotificationResponse,
         withCompletionHandler completionHandler: @escaping () -> Void
     ) {
+        print("background notification: \(response.notification)")
+        
         completionHandler()
     }
 }
