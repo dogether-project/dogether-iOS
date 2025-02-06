d8bef46979026afcda73814ec89d392bbf34e144
seungyooooong <chadange@naver.com>
2025-02-06 23:06:28 +0900

    feat #15: Colors 추가

---
+++ dogether/Resource/Colors.swift
@@ -0,0 +1,32 @@
+//
+//  Colors.swift
+//  dogether
+//
+//  Created by seungyooooong on 2/6/25.
+//
+
+import UIKit
+
+enum Colors {
+    // MARK: Blue
+    static let blue100 = UIColor(named: "Blue-100")
+    static let blue200 = UIColor(named: "Blue-200")
+    static let blue300 = UIColor(named: "Blue-300")
+    static let blue400 = UIColor(named: "Blue-400")
+    
+    // MARK: Grey
+    static let grey0 = UIColor(named: "Grey-0")
+    static let grey50 = UIColor(named: "Grey-50")
+    static let grey100 = UIColor(named: "Grey-100")
+    static let grey200 = UIColor(named: "Grey-200")
+    static let grey300 = UIColor(named: "Grey-300")
+    static let grey400 = UIColor(named: "Grey-400")
+    static let grey500 = UIColor(named: "Grey-500")
+    static let grey600 = UIColor(named: "Grey-600")
+    static let grey700 = UIColor(named: "Grey-700")
+    static let grey800 = UIColor(named: "Grey-800")
+    static let grey900 = UIColor(named: "Grey-900")
+    
+    // MARK: Point
+    static let red = UIColor(named: "RED")
+}
