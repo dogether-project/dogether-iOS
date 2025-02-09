3a8982b00f9af21a44fafe59fff841ad02a30f8f
seungyooooong <chadange@naver.com>
2025-02-09 23:26:18 +0900

    feat #19: DogetherButton 수정

--- dogether/Presentation/Common/DogetherButton.swift
+++ dogether/Presentation/Common/DogetherButton.swift
@@ -14,6 +14,10 @@ class DogetherButton: UIButton {
     var buttonColor: UIColor
     var disabled: Bool
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     init(action: @escaping () -> Void, title: String, buttonColor: UIColor = .blue300, disabled: Bool = false) {
         self.action = action
         self.title = title
@@ -21,19 +25,31 @@ class DogetherButton: UIButton {
         self.disabled = disabled
         
         super.init(frame: .zero)
-        
-        self.setTitle(title, for: .normal)
-        self.setTitleColor(.white, for: .normal)
-        self.titleLabel?.font = Fonts.bold(size: 16)
-        self.addTarget(self, action: #selector(didTapDogetherButton), for: .touchUpInside)
-        
-        self.backgroundColor = buttonColor
-        self.layer.cornerRadius = 12
+        setUI()
     }
     
-    func toggleButtonDisabled() {
-        self.disabled.toggle()
+    private lazy var dogetherButton = {
+        let button = UIButton()
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = Fonts.body1B
+        button.backgroundColor = buttonColor
+        button.layer.cornerRadius = 12
+        return button
+    }()
+    
+    private func setUI() {
+        [dogetherButton].forEach { addSubview($0) }
+        
+        dogetherButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+        
+        dogetherButton.addTarget(self, action: #selector(didTapDogetherButton), for: .touchUpInside)
     }
+    
     func setButtonDisabled(_ disabled: Bool) {
         self.disabled = disabled
     }
@@ -42,9 +58,5 @@ class DogetherButton: UIButton {
         if !disabled {
             action()
         }
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
