fa32234d0746803c66f66c92d971566cb2dacd7d
jieun <jieun_0330@naver.com>
2025-02-12 12:24:48 +0900

    feat #4: 애플 로그인 로직 수정

--- dogether/Domain/UseCases/AppleSignInDelegate.swift
+++ dogether/Domain/UseCases/AppleSignInDelegate.swift
@@ -10,6 +10,17 @@ import AuthenticationServices
 
 final class AppleSignInDelegate: NSObject, ASAuthorizationControllerDelegate {
     
+    private var continuation: CheckedContinuation<(identityToken: String, fullUserName: String), Error>?
+    
+    // 애플 로그인 결과 비동기 반환
+    var signInResult: (identityToken: String, fullUserName: String)? {
+        get async throws {
+            return try await withCheckedThrowingContinuation { continuation in
+                self.continuation = continuation
+            }
+        }
+    }
+    
     // MARK: - 로그인 성공
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         
@@ -19,32 +30,35 @@ final class AppleSignInDelegate: NSObject, ASAuthorizationControllerDelegate {
             
             let userIdentifier = appleIDCrendential.user
             let fullName = appleIDCrendential.fullName
-            guard let identityToken = appleIDCrendential.identityToken else { return }
-            guard let authrizationCode = appleIDCrendential.authorizationCode else { return }
-                        
-            if let firstName = fullName?.givenName,
-               let lastName = fullName?.familyName {
-                let fullUserName = "\(firstName)\(lastName)"
-                UserDefaultsManager.shared.userFullName = fullUserName
-            } else if let firstName = fullName?.givenName {
-                UserDefaultsManager.shared.userFullName = firstName
-            } else if let lastName = fullName?.familyName {
-                UserDefaultsManager.shared.userFullName = lastName
+            
+            guard let identityTokenData = appleIDCrendential.identityToken,
+                  let identityTokenString = String(data: identityTokenData, encoding: .utf8),
+                  let authrizationCode = appleIDCrendential.authorizationCode else {
+                continuation?.resume(throwing: NetworkError.unknown)
+                return
             }
                         
+            let firstName = fullName?.givenName ?? "이름없음"
+            let lastName = fullName?.familyName ?? "이름없음"
+            let fullUserName = firstName + lastName
+            
+            UserDefaultsManager.shared.userFullName = fullUserName
+            
             // TODO: - OSLog로 변경하기
             print("============= 🚀 Login Log 🚀 =============")
             print("✅ 로그인 성공")
             print("사용자 ID: \(userIdentifier)")
-            print("사용자 이름: \(fullName?.givenName ?? "")")
-            print("사용자 Token: \(identityToken)")
+            print("사용자 이름: \(fullUserName)")
+            print("사용자 Token: \(identityTokenString)")
             print("사용자 authorizationCode: \(authrizationCode)")
             print("===========================================")
             
-            // TODO: - 로그인 성공 후 수행할 작업
+            // 비동기 결과 반환
+            continuation?.resume(returning: (identityTokenString, fullUserName))
             
         // MARK: - 암호 기반 인증에 성공한 경우, 사용자의 인증 정보를 확인하고 필요한 작업을 수행합니다
         case let passwordCredential as ASPasswordCredential:
+            
             let userName = passwordCredential.user
             let password = passwordCredential.password
             
@@ -54,10 +68,8 @@ final class AppleSignInDelegate: NSObject, ASAuthorizationControllerDelegate {
             print("사용자 비밀번호: \(password)")
             print("===========================================")
             
-            // TODO: - 로그인 성공 후 수행할 작업
-
         default:
-            break
+            continuation?.resume(throwing: NetworkError.unknown)
         }
     }
 }
---
+++ dogether/Presentation/ViewModel/LoginViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewModel.swift
+//  dogether
+//
+//  Created by 박지은 on 2/11/25.
+//
+
+import Foundation
+import AuthenticationServices
+
+final class LoginViewModel: ObservableObject {
+    
+    private let appleSignInDelegate = AppleSignInDelegate()
+
+    func singInWithApple() async throws -> AppleLoginResponse {
+        
+        // Apple로 로그인을 시작할 메서드(버튼을 눌렀을때 호출할 메서드)
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email] // 유저로부터 알 수 있는 정보들
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        // 인증창을 보여주기 위해 로그인 정보 관련 대리자 설정
+        controller.delegate = appleSignInDelegate
+        controller.performRequests()
+        
+        // 애플 로그인 결과
+        guard let result = try await appleSignInDelegate.signInResult else {
+            throw NetworkError.unknown
+        }
+        
+        let idToken = result.identityToken
+        let name = result.fullUserName
+        
+        // 서버에 로그인 요청 보낼 데이터
+        let loginRequest = AppleLoginRequest(name: name, idToken: idToken)
+        
+        return try await NetworkManager.shared.request(AuthRouter.appleLogin(appleLoginRequest: loginRequest))
+    }
+}
