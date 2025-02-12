b1b6c58f910c1c19bdb298c0f54c7dcb9a45264d
seungyooooong <chadange@naver.com>
2025-02-12 22:21:44 +0900

    feat #20: presignedUrls API 추가

---
+++ dogether/Data/Network/Router/S3.swift
@@ -0,0 +1,50 @@
+//
+//  S3.swift
+//  dogether
+//
+//  Created by seungyooooong on 2/12/25.
+//
+
+import Foundation
+
+enum S3: NetworkEndpoint {
+    case presignedUrls(presignedUrlRequest: PresignedUrlRequest)
+    
+    var path: String {
+        switch self {
+        case .presignedUrls:
+            return Path.api + Path.s3 + "/presigned-urls"
+        }
+    }
+    
+    var method: NetworkMethod {
+        switch self {
+        case .presignedUrls:
+            return .post
+        }
+    }
+    
+    var parameters: [URLQueryItem]? {
+        switch self {
+        default:
+            return nil
+        }
+    }
+    
+    var header: [String : String]? {
+        switch self {
+        default:
+            return [
+                Header.Key.contentType: Header.Value.applicationJson,
+                Header.Key.authorization: Header.Value.bearer + Header.Value.accessToken
+            ]
+        }
+    }
+    
+    var body: (any Encodable)? {
+        switch self {
+        case .presignedUrls(let presignedUrlRequest):
+            return presignedUrlRequest
+        }
+    }
+}
