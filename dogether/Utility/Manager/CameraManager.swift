27be9392ce114f4069e36dcc8f6c94b5979fbece
seungyooooong <chadange@naver.com>
2025-02-12 22:10:22 +0900

    feat #20: CameraManager, GalleryManager 추가

---
+++ dogether/Utility/Manager/CameraManager.swift
@@ -0,0 +1,44 @@
+//
+//  CameraManager.swift
+//  dogether
+//
+//  Created by seungyooooong on 2/12/25.
+//
+
+import Foundation
+import UIKit
+
+final class CameraManager: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    private weak var viewController: UIViewController?
+    var completion: ((UIImage?) -> Void)?
+
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+
+    func openCamera() {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else { return }
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        picker.allowsEditing = false
+        viewController?.present(picker, animated: true)
+    }
+
+    func imagePickerController(
+        _ picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]
+    ) {
+        if let image = info[.originalImage] as? UIImage, let pngData = image.pngData() {
+            let pngImage = UIImage(data: pngData)
+            completion?(pngImage)
+        }
+        picker.dismiss(animated: true)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+    
+}
