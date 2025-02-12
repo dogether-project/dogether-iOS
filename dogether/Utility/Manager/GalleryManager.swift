af226db14180418943cf508619f76018663bdced
seungyooooong <chadange@naver.com>
2025-02-12 22:10:22 +0900

    feat #20: CameraManager, GalleryManager 추가

---
+++ dogether/Utility/Manager/GalleryManager.swift
@@ -0,0 +1,43 @@
+//
+//  GalleryManager.swift
+//  dogether
+//
+//  Created by seungyooooong on 2/12/25.
+//
+
+import Foundation
+import PhotosUI
+import UIKit
+
+final class GalleryManager: NSObject, PHPickerViewControllerDelegate {
+    private weak var viewController: UIViewController?
+    var completion: ((UIImage?) -> Void)?
+
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+
+    func openGallery() {
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 1
+        config.filter = .images
+
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = self
+        viewController?.present(picker, animated: true)
+    }
+
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+
+        guard let provider = results.first?.itemProvider, provider.canLoadObject(ofClass: UIImage.self) else { return }
+        provider.loadObject(ofClass: UIImage.self) { image, _ in
+            DispatchQueue.main.async {
+                if let uiImage = image as? UIImage, let pngData = uiImage.pngData() {
+                    let pngImage = UIImage(data: pngData)
+                    self.completion?(pngImage)
+                }
+            }
+        }
+    }
+}
