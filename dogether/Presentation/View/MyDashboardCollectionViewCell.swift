7907dbf6789a6e3177503d6647f2c3c97bb4f63a
jieun <jieun_0330@naver.com>
2025-02-14 14:07:30 +0900

    feat #35: 내통계 CollectionViewCell 생성

---
+++ dogether/Presentation/View/MyDashboardCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MyDashboardCollectionViewCell.swift
+//  dogether
+//
+//  Created by 박지은 on 2/14/25.
+//
+
+import UIKit
+
+class MyDashboardCollectionViewCell: BaseCollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
