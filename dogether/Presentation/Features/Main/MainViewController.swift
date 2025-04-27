cb08ba6a0a5784456b3957629b3c2445854c96e9
seungyooooong <chadange@naver.com>
2025-04-27 10:28:47 +0900

    feat #17: getGroups 임시 추가

--- dogether/Presentation/Features/Main/MainViewController.swift
+++ dogether/Presentation/Features/Main/MainViewController.swift
@@ -299,11 +299,6 @@ extension MainViewController {
     @objc private func tappedGroupNameStackView() {
         presentBottomSheet()
     }
-    
-    @objc private func tappedGroupNameStackView() {
-        // FIXME: 추후 공용 컴포넌트 group sheet 추가되면 수정
-        print("show group sheet")
-    }
 }
 
 // MARK: - about pan gesture
