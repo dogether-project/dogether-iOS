24a6ed835c80220445ecba3c2b1a52038b380b64
유재홍 <ujhong7@gmail.com>
2025-04-29 16:33:40 +0900

    chore #13:  지은님 코드 (Dashboard) 주석처리

--- dogether/Presentation/Features/Dashboard/Components/MyDashboardCollectionViewCell.swift
+++ dogether/Presentation/Features/Dashboard/Components/MyDashboardCollectionViewCell.swift
@@ -1,68 +1,68 @@
+////
+////  MyDashboardCollectionViewCell.swift
+////  dogether
+////
+////  Created by 박지은 on 2/14/25.
+////
 //
-//  MyDashboardCollectionViewCell.swift
-//  dogether
+//import UIKit
+//import SnapKit
 //
-//  Created by 박지은 on 2/14/25.
+//class MyDashboardCollectionViewCell: BaseCollectionViewCell, ReusableProtocol {
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//    }
+//    required init?(coder: NSCoder) { fatalError() }
+//    
+//    private let icon = UIImageView()
+//    
+//    private let titleLabel = {
+//        let label = UILabel()
+//        label.font = Fonts.body2S
+//        label.textColor = .grey200
+//        label.textAlignment = .center
+//        return label
+//    }()
+//    
+//    private let countLabel = {
+//        let label = UILabel()
+//        label.font = .systemFont(ofSize: 20)
+//        label.textColor = .grey0
+//        label.textAlignment = .center
+//        return label
+//    }()
+//    
+//    override func configureView() { }
+//    
+//    override func configureAction() { }
+//    
+//    override func configureHierarchy() {
+//        [icon, titleLabel, countLabel].forEach { contentView.addSubview($0) }
+//    }
+//    
+//    override func configureConstraints() {
+//        icon.snp.makeConstraints {
+//            $0.top.equalToSuperview().offset(17.5)
+//            $0.centerX.equalToSuperview()
+//            $0.size.equalTo(24)
+//        }
+//        
+//        titleLabel.snp.makeConstraints {
+//            $0.top.equalTo(icon.snp.bottom).offset(12)
+//            $0.centerX.equalToSuperview()
+//        }
+//        
+//        countLabel.snp.makeConstraints {
+//            $0.top.equalTo(titleLabel.snp.bottom).offset(6)
+//            $0.centerX.equalToSuperview()
+//        }
+//    }
+//}
 //
-
-import UIKit
-import SnapKit
-
-class MyDashboardCollectionViewCell: BaseCollectionViewCell, ReusableProtocol {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    required init?(coder: NSCoder) { fatalError() }
-    
-    private let icon = UIImageView()
-    
-    private let titleLabel = {
-        let label = UILabel()
-        label.font = Fonts.body2S
-        label.textColor = .grey200
-        label.textAlignment = .center
-        return label
-    }()
-    
-    private let countLabel = {
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 20)
-        label.textColor = .grey0
-        label.textAlignment = .center
-        return label
-    }()
-    
-    override func configureView() { }
-    
-    override func configureAction() { }
-    
-    override func configureHierarchy() {
-        [icon, titleLabel, countLabel].forEach { contentView.addSubview($0) }
-    }
-    
-    override func configureConstraints() {
-        icon.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(17.5)
-            $0.centerX.equalToSuperview()
-            $0.size.equalTo(24)
-        }
-        
-        titleLabel.snp.makeConstraints {
-            $0.top.equalTo(icon.snp.bottom).offset(12)
-            $0.centerX.equalToSuperview()
-        }
-        
-        countLabel.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(6)
-            $0.centerX.equalToSuperview()
-        }
-    }
-}
-
-extension MyDashboardCollectionViewCell {
-    func configure(with image: UIImage, title: String, count: String) {
-        icon.image = image
-        titleLabel.text = title
-        countLabel.text = count
-    }
-}
+//extension MyDashboardCollectionViewCell {
+//    func configure(with image: UIImage, title: String, count: String) {
+//        icon.image = image
+//        titleLabel.text = title
+//        countLabel.text = count
+//    }
+//}
