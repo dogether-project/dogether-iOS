537bf8c76d3125579cc22014da408daff1861804
유재홍 <ujhong7@gmail.com>
2025-07-08 13:45:34 +0900

    feat #54: 그룹 생성/참여 시 에러 시나리오 테스트 코드 추가

--- dogether/Data/RepositoryTest/GroupRepositoryTest.swift
+++ dogether/Data/RepositoryTest/GroupRepositoryTest.swift
@@ -9,14 +9,52 @@ import Foundation
 
 final class GroupRepositoryTest: GroupProtocol {
     func createGroup(createGroupRequest: CreateGroupRequest) async throws -> CreateGroupResponse {
+        if createGroupRequest.groupName == "1" {
+            throw NetworkError.badRequest
+        }
+        
+        if createGroupRequest.groupName == "2" {
+            throw NetworkError.noData
+        }
+        
+        if createGroupRequest.groupName == "3" {
+            throw NetworkError.serverError(message: "서버 에러가 발생했습니다.")
+        }
+            
         return CreateGroupResponse(joinCode: "000000")
     }
     
     func joinGroup(joinGroupRequest: JoinGroupRequest) async throws -> JoinGroupResponse {
-        // MARK: - TEST: 99999999 입력 시 에러 발생 테스트
-        if joinGroupRequest.joinCode == "99999999" {
-            throw NetworkError.unknown(NSError(domain: "Test", code: -1, userInfo: [NSLocalizedDescriptionKey: "유효하지 않은 초대코드"]))
+        let code = joinGroupRequest.joinCode
+        
+        if code == "00000000" {
+            throw NetworkError.dogetherError(code: .CGF0004, message: "유효하지 않은 그룹입니다.")
         }
+        
+        if code == "11111111" {
+            throw NetworkError.dogetherError(code: .CGF0007, message: "이미 참여한 그룹입니다.")
+        }
+        
+        if code == "22222222" {
+            throw NetworkError.dogetherError(code: .CGF0009, message: "종료된 그룹입니다.")
+        }
+        
+        if code == "33333333" {
+            throw NetworkError.dogetherError(code: .CGF0008, message: "그룹 인원이 가득 찼습니다.")
+        }
+        
+        if code == "44444444" {
+            throw NetworkError.serverError(message: "서버에러")
+        }
+        
+        if code == "55555555" {
+            throw NetworkError.decodingFailed
+        }
+        
+        if code == "66666666" {
+            throw NetworkError.noData
+        }
+        
         return JoinGroupResponse(groupName: "testGroup", duration: 3, maximumMemberCount: 10, startAt: "2025-01-01", endAt: "2025-01-04")
     }
     
