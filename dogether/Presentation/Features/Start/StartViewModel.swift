fabd3f01014729a147678e6531c8a7f23d1d2ce8
seungyooooong <chadange@naver.com>
2025-08-20 22:55:20 +0900

    feat #48: StartView 구현 & ViewDatas 적용

---
+++ dogether/Presentation/Features/Start/StartViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  StartViewModel.swift
+//  dogether
+//
+//  Created by seungyooooong on 8/19/25.
+//
+
+import RxRelay
+
+final class StartViewModel {
+    private(set) var isFirstGroup = BehaviorRelay<Bool>(value: true)
+}
