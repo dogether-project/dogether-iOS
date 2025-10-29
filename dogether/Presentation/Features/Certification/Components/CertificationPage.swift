fb465aefe1bc4b3e3ba7e215052457d5701fc693
seungyooooong <chadange@naver.com>
2025-10-29 18:13:01 +0900

    feat #111: certificationScrollView 추가

--- dogether/Presentation/Features/Certification/Components/CertificationPage.swift
+++ dogether/Presentation/Features/Certification/Components/CertificationPage.swift
@@ -18,6 +18,8 @@ final class CertificationPage: BasePage {
     
     private let navigationHeader = NavigationHeader(title: "인증 정보")
     private let thumbnailListView = ThumbnailListView()
+    private let certificationScrollView = UIScrollView()
+    private let certificationStackView = UIStackView()
     private let certificationListView = CertificationListView()
     private let statusView = TodoStatusButton(type: .waitCertification)
     private let contentLabel = UILabel()
@@ -25,6 +27,17 @@ final class CertificationPage: BasePage {
     private let certificateButton = DogetherButton(title: "인증하기")
     
     override func configureView() {
+        certificationScrollView.showsVerticalScrollIndicator = false
+        
+        [certificationListView, statusView, contentLabel, reviewFeedbackView].forEach {
+            certificationStackView.addArrangedSubview($0)
+        }
+        certificationStackView.axis = .vertical
+        certificationStackView.alignment = .center
+        certificationStackView.setCustomSpacing(32, after: certificationListView)
+        certificationStackView.setCustomSpacing(8, after: statusView)
+        certificationStackView.setCustomSpacing(16, after: contentLabel)
+        
         contentLabel.textColor = .grey0
         contentLabel.numberOfLines = 0
         
@@ -36,9 +49,8 @@ final class CertificationPage: BasePage {
     }
     
     override func configureHierarchy() {
-        [navigationHeader, thumbnailListView, certificationListView,
-         statusView, contentLabel, reviewFeedbackView, certificateButton
-        ].forEach { addSubview($0) }
+        [navigationHeader, thumbnailListView, certificationScrollView, certificateButton].forEach { addSubview($0) }
+        certificationScrollView.addSubview(certificationStackView)
     }
      
     override func configureConstraints() {
@@ -53,25 +65,32 @@ final class CertificationPage: BasePage {
             $0.height.equalTo(54)
         }
         
+        certificationScrollView.snp.makeConstraints {
+            $0.top.equalTo(thumbnailListView.snp.bottom).offset(12)
+            $0.bottom.horizontalEdges.equalToSuperview()
+        }
+        
+        certificationStackView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.verticalEdges.equalToSuperview()
+        }
+        
         certificationListView.snp.makeConstraints {
-            $0.top.equalTo(thumbnailListView.snp.bottom).offset(20)
+            $0.top.equalToSuperview().offset(8)
             $0.horizontalEdges.equalToSuperview()
             $0.height.equalTo(frame.width - 32)
         }
         
         statusView.snp.makeConstraints {
             $0.centerX.equalToSuperview()
-            $0.top.equalTo(certificationListView.snp.bottom).offset(32)
             $0.height.equalTo(32)
         }
         
         contentLabel.snp.makeConstraints {
-            $0.top.equalTo(statusView.snp.bottom).offset(8)
             $0.horizontalEdges.equalToSuperview().inset(16)
         }
         
         reviewFeedbackView.snp.makeConstraints {
-            $0.top.equalTo(contentLabel.snp.bottom).offset(16)
             $0.horizontalEdges.equalToSuperview().inset(16)
         }
         
