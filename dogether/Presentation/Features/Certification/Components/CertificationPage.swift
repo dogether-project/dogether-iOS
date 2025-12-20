ec92a81936f3eb905a0ce8e6944578b7a15201d6
seungyooooong <chadange@naver.com>
2025-12-20 18:44:54 +0900

    fix: 과거 미인증 투두에서 인증하기 버튼이 표시되는 버그 발견, 임시로 인증하기 버튼 삭제

--- dogether/Presentation/Features/Certification/Components/CertificationPage.swift
+++ dogether/Presentation/Features/Certification/Components/CertificationPage.swift
@@ -12,12 +12,12 @@ final class CertificationPage: BasePage {
         didSet {
             thumbnailListView.delegate = delegate
             certificationListView.delegate = delegate
-            certificateButton.addAction(
-                UIAction { [weak self] _ in
-                    guard let self, let currentTodo else { return }
-                    delegate?.goCertificateViewAction(todo: currentTodo)
-                }, for: .touchUpInside
-            )
+//            certificateButton.addAction(
+//                UIAction { [weak self] _ in
+//                    guard let self, let currentTodo else { return }
+//                    delegate?.goCertificateViewAction(todo: currentTodo)
+//                }, for: .touchUpInside
+//            )
         }
     }
     
@@ -29,7 +29,7 @@ final class CertificationPage: BasePage {
     private let statusView = TodoStatusButton()
     private let contentLabel = UILabel()
     private let reviewFeedbackView = ReviewFeedbackView()
-    private let certificateButton = DogetherButton("인증하기")
+//    private let certificateButton = DogetherButton("인증하기")
     
     private var currentTodo: TodoEntity?
     
@@ -54,7 +54,7 @@ final class CertificationPage: BasePage {
     }
     
     override func configureHierarchy() {
-        [navigationHeader, thumbnailListView, certificationScrollView, certificateButton].forEach { addSubview($0) }
+        [navigationHeader, thumbnailListView, certificationScrollView/*, certificateButton*/].forEach { addSubview($0) }
         certificationScrollView.addSubview(certificationStackView)
     }
      
@@ -98,9 +98,9 @@ final class CertificationPage: BasePage {
             $0.horizontalEdges.equalToSuperview().inset(16)
         }
         
-        certificateButton.snp.makeConstraints {
-            $0.bottom.horizontalEdges.equalToSuperview().inset(16)
-        }
+//        certificateButton.snp.makeConstraints {
+//            $0.bottom.horizontalEdges.equalToSuperview().inset(16)
+//        }
     }
     
     // MARK: - updateView
@@ -123,9 +123,9 @@ final class CertificationPage: BasePage {
             reviewFeedbackView.updateView(datas.todos[datas.index].reviewFeedback ?? "")
             
             // FIXME: 추후 수정
-            var dogetherButtonViewDatas = certificateButton.currentViewDatas ?? DogetherButtonViewDatas()
-            dogetherButtonViewDatas.isHidden = datas.rankingEntity != nil || datas.todos[datas.index].status != .waitCertification
-            certificateButton.updateView(dogetherButtonViewDatas)
+//            var dogetherButtonViewDatas = certificateButton.currentViewDatas ?? DogetherButtonViewDatas()
+//            dogetherButtonViewDatas.isHidden = datas.rankingEntity != nil || datas.todos[datas.index].status != .waitCertification
+//            certificateButton.updateView(dogetherButtonViewDatas)
         }
     }
 }
